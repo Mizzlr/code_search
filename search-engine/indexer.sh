a443e8ef99b42147d01dfe9d08e7fb5a452ac8ed
Mizzlr <mushtaque@codenation.co.in>
2018-06-06 15:34:04 +0530

    Added dockerfile stuff.

--- search-engine/indexer.sh
+++ search-engine/indexer.sh
@@ -1,7 +1,7 @@
 set -x
 unzip -r storage/uploads/*.zip -d storage/uploads
-fkill -9 "python3 codesearch/code/indexer.py storage/uploads storage/index"
-fkill -9 "python3 codesearch/query/searcher.py storage/index"
+ps -ef | grep codesearch/query/searcher.py | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | grep codesearch/code/indexer.py | grep -v grep | awk '{print $2}' | xargs kill -9
 rm storage/uploads/*.zip
 rm storage/index/*
 python3 codesearch/code/indexer.py storage/uploads storage/index
